5a613913b2df51f3fe2cdbf0fd3f8281a744de16
Aadit Meenege <70190456+aadit-meenege123@users.noreply.github.com>
2026-07-26 21:40:21 -0700

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        topKMap = {}
+        for num in nums:
+            topKMap[num] = topKMap.get(num,0) + 1
+
+        sortedList = sorted(topKMap.items(), key=lambda x:x[1], reverse=True)
+
+        finalFrequentInt = []
+
+        for pair in sortedList[:k] :
+            finalFrequentInt.append(pair[0]) 
+
+        return finalFrequentInt
+
+        #LETS USE BUCKET SORT OR HEAP STRATEGY here next time
